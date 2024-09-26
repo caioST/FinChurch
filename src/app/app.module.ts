e0380a0e6728342a908acd22f84cd8c4030043a0
caioST <dasilvacaio919@gmail.com>
2024-09-25 22:17:17 -0300

    alteracoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx'; // Certifique-se de usar '/ngx'
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx'; 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, FingerprintAIO],
   bootstrap: [AppComponent],
 })
